3980946445eab0ecfe044afa8b1084be3fb93086
arike-liasu <aishatliasu17965@gmail.com>
2021-08-24 14:07:06 +0100

    feat: adds starter files

---
+++ script.js
@@ -0,0 +1,9 @@
+let popup = document.querySelector(".popup");
+
+let profileEditButton = document.querySelector(".profile__edit-button");
+
+function actionAfterClick() {
+
+}
+
+profileEditButton.addEventListener("click", actionAfterClick)
