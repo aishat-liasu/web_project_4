32d34d558650eab04e8c719b45893c3d01e941aa
arike-liasu <aishatliasu17965@gmail.com>
2021-08-24 14:46:26 +0100

    feat: add popup control functionality

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
 let popup = document.querySelector(".popup");
+let popupCloseButton = document.querySelector(".popup__close-button");
+let popupSaveButton = document.querySelector(".popup__save-button");
+
+let profileTitle = document.querySelector(".profile__title");
+let profileSubtitle = document.querySelector(".profile__subtitle");
+
+let popupFieldTitle = document.querySelector(".popup__field_el_title");
+let popupFieldSubtitle = document.querySelector(".popup__field_el_subtitle")
+
+popupFieldSubtitle.textContent = profileSubtitle.textContent;
+popupFieldTitle.textContent = profileTitle.textContent;
 
 let profileEditButton = document.querySelector(".profile__edit-button");
 
-function actionAfterClick() {
-
+function showPopup() {
+  popup.classList.add("popup_opened")
+  console.log(popup.classList)
 }
 
-profileEditButton.addEventListener("click", actionAfterClick)
+function closePopup(e) {
+  e.preventDefault();
+  console.log("Here")
+  popup.classList.remove("popup_opened")
+  console.log(popup.classList)
+}
+
+profileEditButton.addEventListener("click", showPopup)
+popupCloseButton.addEventListener("click", closePopup)
