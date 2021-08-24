db8a338e28bb073f41ae500c2bc4300f9e9ee7e8
arike-liasu <aishatliasu17965@gmail.com>
2021-08-24 17:20:22 +0100

    feat: effects the save functionality of the popup

--- script.js
+++ script.js
@@ -6,24 +6,41 @@ let profileTitle = document.querySelector(".profile__title");
 let profileSubtitle = document.querySelector(".profile__subtitle");
 
 let popupFieldTitle = document.querySelector(".popup__field_el_title");
-let popupFieldSubtitle = document.querySelector(".popup__field_el_subtitle")
+let popupFieldSubtitle = document.querySelector(".popup__field_el_subtitle");
 
-popupFieldSubtitle.textContent = profileSubtitle.textContent;
-popupFieldTitle.textContent = profileTitle.textContent;
+console.log(profileSubtitle.textContent);
+console.log(profileTitle.textContent);
+
+console.log(popupFieldTitle.textContent);
+console.log(popupFieldSubtitle.textContent);
 
 let profileEditButton = document.querySelector(".profile__edit-button");
 
 function showPopup() {
-  popup.classList.add("popup_opened")
-  console.log(popup.classList)
+  popup.classList.add("popup_opened");
+  console.log(popup.classList);
+  console.log(popupFieldTitle.textContent);
+  console.log(popupFieldSubtitle.textContent);
+  popupFieldSubtitle.value = profileSubtitle.textContent;
+  popupFieldTitle.value = profileTitle.textContent;
+  console.log(popupFieldTitle.textContent);
+  console.log(popupFieldSubtitle.textContent);
 }
 
 function closePopup(e) {
   e.preventDefault();
-  console.log("Here")
-  popup.classList.remove("popup_opened")
-  console.log(popup.classList)
+  console.log("Here");
+  popup.classList.remove("popup_opened");
+  console.log(popup.classList);
 }
 
-profileEditButton.addEventListener("click", showPopup)
-popupCloseButton.addEventListener("click", closePopup)
+function saveInputs(e) {
+  e.preventDefault();
+  profileTitle.textContent = popupFieldTitle.value;
+  profileSubtitle.textContent = popupFieldSubtitle.value;
+  popup.classList.remove("popup_opened");
+}
+
+profileEditButton.addEventListener("click", showPopup);
+popupCloseButton.addEventListener("click", closePopup);
+popupSaveButton.addEventListener("click", saveInputs);
