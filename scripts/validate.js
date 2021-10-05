481d3ccb270ebb1a571f348540c20b8a06ffae64
arike-liasu <aishatliasu17965@gmail.com>
2021-10-05 10:50:17 +0100

    refactor: remove duplicated functions

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,24 +1,8 @@
-const hideFieldError = (formElement, fieldElement) => {
-  const errorElement = formElement.querySelector(`#${fieldElement.id}-error`);
-
-  fieldElement.classList.remove("popup__field_type_error");
-  errorElement.classList.remove("popup__field-error_active");
-  errorElement.textContent = "";
-};
-
-//clears the error indicators and resets the form
-const resetForm = (popupElement) => {
+// resets the form
+export const resetForm = (popupElement) => {
   const popupForm = popupElement.querySelector(".popup__form");
-  const popupFieldList = Array.from(
-    popupElement.querySelectorAll(".popup__field")
-  );
-  const buttonElement = popupElement.querySelector(".popup__submit-button");
 
-  popupFieldList.forEach((fieldElement) => {
-    hideFieldError(popupForm, fieldElement);
-  });
-  popupForm.reset();
-
-  buttonElement.classList.add("popup__submit-button_disabled");
-  buttonElement.setAttribute("disabled", true);
+  if (popupForm) {
+    popupForm.reset();
+  }
 };
