cb549efd12a4f32d1e79a6de4f9c6fd0de096a18
arike-liasu <aishatliasu17965@gmail.com>
2021-10-20 21:31:55 +0100

    refactor: create a utils folder

---
+++ scripts/utils/utils.js
@@ -0,0 +1,37 @@
+const profileTitle = document.querySelector(".profile__title");
+const profileSubtitle = document.querySelector(".profile__subtitle");
+
+const popupFieldTitle = document.querySelector(".popup__field_el_title");
+const popupFieldSubtitle = document.querySelector(".popup__field_el_subtitle");
+
+const profileAddButton = document.querySelector(".profile__add-button");
+const popupTypeAdd = document.querySelector(".popup_type_add");
+const popupTypeAddCloseButton = document.querySelector(
+  ".popup_type_add .popup__close-button"
+);
+const popupTypeAddSaveButton = document.querySelector(
+  ".popup_type_add .popup__submit-button"
+);
+
+const popupFieldPlaceTitle = document.querySelector(
+  ".popup__field_el_place-title"
+);
+const popupFieldPlaceImageURL = document.querySelector(
+  ".popup__field_el_place-image-url"
+);
+
+const popupTypeImage = document.querySelector(".popup_type_image");
+const popupImage = document.querySelector(".popup__image");
+const popupImageLocation = document.querySelector(".popup__image-location");
+const popupTypeImageCloseButton = document.querySelector(
+  ".popup_type_image .popup__close-button"
+);
+
+const profileEditButton = document.querySelector(".profile__edit-button");
+const popupTypeEdit = document.querySelector(".popup_type_edit");
+const popupTypeEditCloseButton = document.querySelector(
+  ".popup_type_edit .popup__close-button"
+);
+const popupTypeEditSaveButton = document.querySelector(
+  ".popup_type_edit .popup__submit-button"
+);
