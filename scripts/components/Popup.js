6d34cc92d1542a76a5151b3bdfdc73400c6ecb17
arike-liasu <aishatliasu17965@gmail.com>
2021-10-21 19:06:08 +0100

    refactor: moves some private methods in Card class to Popup class

--- scripts/components/Popup.js
+++ scripts/components/Popup.js
@@ -2,25 +2,42 @@ export default class Popup {
   constructor(popupSelector) {
     this._popupElement = document.querySelector(popupSelector);
   }
-  open() {
-    this._popupElement.classList.add("popup_opened");
-  }
-  close() {
-    this._popupElement.classList.remove("popup_opened");
-  }
+
   _handleEscClose(evt) {
     if (evt.key === "Escape") {
       this.close();
     }
   }
-  _handleClickOutsideForm(evt) {
+  //adds event to the document
+  _addEventToDocument() {
+    document.addEventListener("keydown", (e) => {
+      this._handleEscClose(e);
+    });
+  }
+
+  //removes event to the document
+  _removeEventFromDocument() {
+    document.removeEventListener("keydown", this._handleEscClose);
+  }
+
+  open() {
+    this._popupElement.classList.add("popup_opened");
+    this._addEventToDocument();
+  }
+
+  close() {
+    this._popupElement.classList.remove("popup_opened");
+    this._removeEventFromDocument();
+  }
+
+  _handleClickOutsidePopup(evt) {
     if (evt.target.classList.contains(evt.target.id)) {
       this.close();
     }
   }
   setEventListeners() {
     this._popupElement.addEventListener("click", (evt) => {
-      this._handleClickOutsideForm(evt);
+      this._handleClickOutsidePopup(evt);
     });
 
     this._popupElement
