31c7308bc82fa058d3055f5765ec7529993ae290
arike-liasu <aishatliasu17965@gmail.com>
2021-10-21 19:03:47 +0100

    refactor: updates the public methods in UserInfo class

--- scripts/components/UserInfo.js
+++ scripts/components/UserInfo.js
@@ -1,8 +1,24 @@
-class UserInfo {
-  constructor({ userName, userJob }) {
-    this._userName = userName;
-    this._userJob = userJob;
+const profileTitle = document.querySelector(".profile__title");
+const profileSubtitle = document.querySelector(".profile__subtitle");
+
+const popupFieldTitle = document.querySelector(".popup__field_el_title");
+const popupFieldSubtitle = document.querySelector(".popup__field_el_subtitle");
+
+export default class UserInfo {
+  constructor({ nameSelector, jobSelector }) {
+    this._nameElement = document.querySelector(nameSelector);
+    this._jobElement = document.querySelector(jobSelector);
   }
-  getUserInfo() {}
-  setUserInfo() {}
+  getUserInfo() {
+    popupFieldTitle.value = this._nameElement.textContent;
+    popupFieldSubtitle.value = this._jobElement.textContent;
+    return {
+      name: this._nameElement.textContent,
+      job: this._jobElement.textContent,
+    };
+  }
+  setUserInfo() {
+    this._nameElement.textContent = popupFieldTitle.value;
+    this._jobElement.textContent = popupFieldSubtitle.value;
+  }
 }
