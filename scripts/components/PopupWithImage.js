e5b092a5f08f19fbac1f1398bae6efa5075dc3db
arike-liasu <aishatliasu17965@gmail.com>
2021-10-21 19:04:38 +0100

    style: removes redundant elements

--- scripts/components/PopupWithImage.js
+++ scripts/components/PopupWithImage.js
@@ -1,11 +1,7 @@
-import Popup from "./Popup";
+import Popup from "./Popup.js";
 
-const popupTypeImage = document.querySelector(".popup_type_image");
 const popupImage = document.querySelector(".popup__image");
 const popupImageLocation = document.querySelector(".popup__image-location");
-const popupTypeImageCloseButton = document.querySelector(
-  ".popup_type_image .popup__close-button"
-);
 
 export default class PopupWithImage extends Popup {
   constructor({ name, link }, popupSelector) {
@@ -16,7 +12,6 @@ export default class PopupWithImage extends Popup {
 
   open() {
     super.open();
-
     popupImage.src = this._link;
     popupImage.alt = `${this._name} picture`;
     popupImageLocation.textContent = this._name;
