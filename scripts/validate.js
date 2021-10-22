42ee93a8ebaf447540eccf706ba1597135f1da6a
arike-liasu <aishatliasu17965@gmail.com>
2021-10-22 08:47:46 +0100

    refactor: remove the validate.js file

--- scripts/validate.js
+++
@@ -1,8 +0,0 @@
-// resets the form
-export const resetForm = (popupElement) => {
-  const popupForm = popupElement.querySelector(".popup__form");
-
-  if (popupForm) {
-    popupForm.reset();
-  }
-};
