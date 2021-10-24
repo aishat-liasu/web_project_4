020f037f708b37551d3e004fb625e08ca7dd00e3
arike-liasu <aishatliasu17965@gmail.com>
2021-10-24 08:53:57 +0100

    refactor: export constants from utils file

--- src/scripts/utils/utils.js
+++ src/scripts/utils/utils.js
@@ -1,37 +1,49 @@
-const profileTitle = document.querySelector(".profile__title");
-const profileSubtitle = document.querySelector(".profile__subtitle");
-
-const popupFieldTitle = document.querySelector(".popup__field_el_title");
-const popupFieldSubtitle = document.querySelector(".popup__field_el_subtitle");
-
-const profileAddButton = document.querySelector(".profile__add-button");
-const popupTypeAdd = document.querySelector(".popup_type_add");
-const popupTypeAddCloseButton = document.querySelector(
-  ".popup_type_add .popup__close-button"
-);
-const popupTypeAddSaveButton = document.querySelector(
-  ".popup_type_add .popup__submit-button"
+export const popupFieldTitle = document.querySelector(".popup__field_el_title");
+export const popupFieldSubtitle = document.querySelector(
+  ".popup__field_el_subtitle"
 );
 
-const popupFieldPlaceTitle = document.querySelector(
+export const profileAddButton = document.querySelector(".profile__add-button");
+
+export const popupFieldPlaceTitle = document.querySelector(
   ".popup__field_el_place-title"
 );
-const popupFieldPlaceImageURL = document.querySelector(
+export const popupFieldPlaceImageURL = document.querySelector(
   ".popup__field_el_place-image-url"
 );
 
-const popupTypeImage = document.querySelector(".popup_type_image");
-const popupImage = document.querySelector(".popup__image");
-const popupImageLocation = document.querySelector(".popup__image-location");
-const popupTypeImageCloseButton = document.querySelector(
-  ".popup_type_image .popup__close-button"
+export const popupImage = document.querySelector(".popup__image");
+export const popupImageLocation = document.querySelector(
+  ".popup__image-location"
 );
 
-const profileEditButton = document.querySelector(".profile__edit-button");
-const popupTypeEdit = document.querySelector(".popup_type_edit");
-const popupTypeEditCloseButton = document.querySelector(
-  ".popup_type_edit .popup__close-button"
+export const profileEditButton = document.querySelector(
+  ".profile__edit-button"
 );
-const popupTypeEditSaveButton = document.querySelector(
-  ".popup_type_edit .popup__submit-button"
-);
+
+export const initialCards = [
+  {
+    name: "Yosemite Valley",
+    link: "https://code.s3.yandex.net/web-code/yosemite.jpg",
+  },
+  {
+    name: "Lake Louise",
+    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg",
+  },
+  {
+    name: "Bald Mountains",
+    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://code.s3.yandex.net/web-code/latemar.jpg",
+  },
+  {
+    name: "Vanoise National Park",
+    link: "https://code.s3.yandex.net/web-code/vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://code.s3.yandex.net/web-code/lago.jpg",
+  },
+];
