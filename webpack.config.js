8b0472c92e72dc5b669403a96a12e8293e630251
arike-liasu <aishatliasu17965@gmail.com>
2021-10-24 08:53:20 +0100

    setup: include configuration files

---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+// connect mini-css-extract-plugin to the project
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  devtool: "inline-source-map",
+  entry: {
+    main: "./src/scripts/index.js",
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"), // you could name this anything you want, but let's stick to 'dist'
+    filename: "main.js", // you could also name this anything you want, but let's stick to 'main.js'
+    publicPath: "",
+  },
+  target: ["web", "es5"], // ensure the Webpack glue code is ES5 compatible too
+  stats: { children: true },
+  mode: "development",
+  devServer: {
+    //: path.resolve(__dirname, "./dist"), // specifies a folder from where to serve the application and its contents
+    compress: true, // this will speed up file loading in development mode
+    port: 8080, // will open your site at localhost:8080 (you can use another port)
+    open: true, // site will open automatically in the browser after executing npm run dev
+    static: {
+      directory: path.join(__dirname, "./dist"),
+    },
+  },
+  module: {
+    rules: [
+      {
+        // a regular expression that searches for all js files
+        test: /\.js$/,
+        // all files must be processed by babel-loader
+        loader: "babel-loader",
+        // exclude the node_modules folder, we don't need to process files in it
+        exclude: "/node_modules/",
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: { importLoaders: 1 },
+          },
+          // add postcss-loader
+          "postcss-loader",
+        ],
+      },
+      {
+        // add the rule for processing files
+        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html", // path to our index.html file
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
+  ],
+};
+
+// module.exports is the syntax for export in Node.js
