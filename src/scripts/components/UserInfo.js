7aed5c459c074845727d526d40c62d989c79663d
arike-liasu <aishatliasu17965@gmail.com>
2021-10-26 11:27:53 +0100

    refactor: update public methods

--- src/scripts/components/UserInfo.js
+++ src/scripts/components/UserInfo.js
@@ -1,20 +1,20 @@
-import { popupFieldTitle, popupFieldSubtitle } from "../utils/utils";
-
 export default class UserInfo {
   constructor({ nameSelector, jobSelector }) {
     this._nameElement = document.querySelector(nameSelector);
     this._jobElement = document.querySelector(jobSelector);
   }
+
+  //display user's data in the form
   getUserInfo() {
-    popupFieldTitle.value = this._nameElement.textContent;
-    popupFieldSubtitle.value = this._jobElement.textContent;
-    /*     return {
+    return {
       name: this._nameElement.textContent,
       job: this._jobElement.textContent,
-    }; */
+    };
   }
-  setUserInfo() {
-    this._nameElement.textContent = popupFieldTitle.value;
-    this._jobElement.textContent = popupFieldSubtitle.value;
+  //add new data to the page
+  setUserInfo({ profileName, profileJob }) {
+    console.log(profileName, profileJob);
+    this._nameElement.textContent = profileName;
+    this._jobElement.textContent = profileJob;
   }
 }
